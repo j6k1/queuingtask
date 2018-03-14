74cf95013c290482d7d95d63ceb0f2e5c24086ac
jinpu <jinpu@LAPTOP-J1SUAON8>
2018-03-15 07:49:06 +0900

    initial commit.

---
+++ src/lib.rs
@@ -0,0 +1,96 @@
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::sync::MutexGuard;
+use std::sync::PoisonError;
+use std::sync::mpsc;
+use std::sync::mpsc::Sender;
+use std::collections::HashMap;
+
+pub enum Notify {
+	Started(u64),
+	Go,
+	Terminated(u64),
+}
+pub struct ThreadQueue {
+	sender_map:Arc<Mutex<HashMap<u64,Sender<Notify>>>>,
+	sender:Option<Sender<Notify>>,
+	current_id:u64,
+}
+impl ThreadQueue {
+	pub fn new() -> ThreadQueue {
+		ThreadQueue {
+			sender_map:Arc::new(Mutex::new(HashMap::new())),
+			sender:None,
+			current_id:0,
+		}
+	}
+
+	pub fn submit<F>(&mut self,f:F) ->
+		Result<(),PoisonError<MutexGuard<HashMap<u64,Sender<Notify>>>>>
+		where F: Fn(), Arc<F>: Send + 'static  {
+
+		if self.sender_map.lock()?.len() == 0 {
+			let (ss,sr) = mpsc::channel();
+			let current_id = Arc::new(Mutex::new(self.current_id));
+			let sender_map = self.sender_map.clone();
+
+			std::thread::spawn(move || {
+				while sender_map.lock().unwrap().len() > 0 {
+					let id = sr.recv().unwrap();
+
+					match id {
+						Notify::Started(id) => {
+							match current_id.lock() {
+								Ok(ref mut current_id) if id == **current_id => {
+									sender_map.lock()
+										.unwrap().get(&id)
+										.unwrap().send(Notify::Go).unwrap();
+								},
+								Ok(_) => (),
+								Err(ref e) => {
+									panic!(format!("{:?}",e));
+								}
+							}
+						},
+						Notify::Terminated(id) => {
+							match current_id.lock() {
+								Ok(ref mut current_id) if id == **current_id => {
+									**current_id = **current_id + 1;
+								},
+								Ok(_) => (),
+								Err(ref e) => {
+									panic!(format!("{:?}",e));
+								}
+							}
+						},
+						_ => (),
+					}
+				}
+			});
+			self.sender = Some(ss)
+		}
+
+		let (cs,cr) = mpsc::channel();
+
+		let ss = match self.sender {
+			Some(ref ss) => ss.clone(),
+			None => panic!("Sender is not initialized."),
+		};
+
+		self.sender_map.lock()?.insert(self.current_id, cs.clone());
+
+		let f = Arc::new(f);
+		let id = self.current_id;
+
+		std::thread::spawn(move || {
+			ss.send(Notify::Started(id)).unwrap();
+			cr.recv().unwrap();
+
+			f();
+
+			cs.send(Notify::Terminated(id)).unwrap();
+		});
+
+		Ok(())
+	}
+}
