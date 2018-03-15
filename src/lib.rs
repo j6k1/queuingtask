8c26307e6dbe6ad31094914278877e7ffb155c84
jinpu <jinpu@LAPTOP-J1SUAON8>
2018-03-15 20:19:33 +0900

    現在実行中対象としているスレッドのIDと新たに発行する最新のIDは同じではないのでlast_idフィールドを設けて分離した。 スレッドの終了通知が来た時に次のスレッドの開始通知を送っていなかったので送るようにした。 スレッドの終了通知が来たときにそのスレッドのSenderをハッシュマップから削除するようにした。

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,7 @@ pub struct ThreadQueue {
 	sender_map:Arc<Mutex<HashMap<u64,Sender<Notify>>>>,
 	sender:Option<Sender<Notify>>,
 	current_id:u64,
+	last_id:u64,
 }
 impl ThreadQueue {
 	pub fn new() -> ThreadQueue {
@@ -22,6 +23,7 @@ impl ThreadQueue {
 			sender_map:Arc::new(Mutex::new(HashMap::new())),
 			sender:None,
 			current_id:0,
+			last_id:0,
 		}
 	}
 
@@ -55,7 +57,18 @@ impl ThreadQueue {
 						Notify::Terminated(id) => {
 							match current_id.lock() {
 								Ok(ref mut current_id) if id == **current_id => {
-									**current_id = **current_id + 1;
+									match sender_map.lock() {
+										Ok(mut map) => {
+											map.remove(&id);
+											let id = id + 1;
+											**current_id = id;
+												map.get(&id)
+													.unwrap().send(Notify::Go).unwrap();
+										},
+										Err(ref e) => {
+											panic!(format!("{:?}",e));
+										}
+									};
 								},
 								Ok(_) => (),
 								Err(ref e) => {
@@ -80,7 +93,9 @@ impl ThreadQueue {
 		self.sender_map.lock()?.insert(self.current_id, cs.clone());
 
 		let f = Arc::new(f);
-		let id = self.current_id;
+		let id = self.last_id;
+
+		self.last_id = self.last_id + 1;
 
 		std::thread::spawn(move || {
 			ss.send(Notify::Started(id)).unwrap();
